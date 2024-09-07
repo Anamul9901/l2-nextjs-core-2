41c8b4c15e8a3631b74700df5fa23ff8936f5d48
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-07 13:17:07 +0600

    blog page no-store data

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,30 @@
+import BlogCard from "@/components/ui/BlogCard";
+import { Blog } from "@/types";
+
+const BlogsPage = async () => {
+  const res = await fetch("http://localhost:5000/blogs", {
+    cache: "no-store", // build time e create korbe nah. prote user er request e new kore build hobe
+  });
+  const blogs = await res.json();
+  return (
+    <div className="w-[90%] mx-auto ">
+      <h1 className="text-4xl text-center my-5">
+        All Blogs From <span className="text-accent">Blogiz</span>
+      </h1>
+      <p className="text-xl text-center text-gray-400 w-2/4 mx-auto">
+        Dive into the{" "}
+        <i>
+          fascinating world of quantum computing, where unlocking unprecedented
+          computational power.
+        </i>
+      </p>
+      <div className="grid grid-cols-3 gap-4 my-10">
+        {blogs.map((blog: Blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
 
 const HomePage = async () => {
-  const res = await fetch("http://localhost:5000/blogs");
+  const res = await fetch("http://localhost:5000/blogs", {
+    next: {
+      revalidate: 30, // 30s por por data build kore rakbe.
+    },
+  });
   const blogs = await res.json();
   // console.log(blogs);
   return (
