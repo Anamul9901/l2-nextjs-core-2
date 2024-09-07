48cfb956ff527b6dca56f49beff881bb9fb2e823
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-07 14:37:03 +0600

    some single blogs ssg with generateStaticParams

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,4 +1,5 @@
 import BlogDetails from "@/components/ui/BlogDetails";
+import { Blog } from "@/types";
 
 interface BlogId {
   params: {
@@ -6,10 +7,20 @@ interface BlogId {
   };
 }
 
+//* generateStaticParams function er maddome bole dete parbo kun kun single data gulu ssg (server e age e er html file creat kore rakbe). aikhane 1st 3 ta ssg korese
+//* ai function er nam 'generateStaticParams' e dete hobe
+export const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
 const BlogDetailsPage = async ({ params }: BlogId) => {
   //   console.log(params);
   const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
-    cache: "no-store", // aikhane data cash kore rakha jabe nah. karon single data er khetre user er request e data deky 
+    cache: "no-store", // aikhane data cash kore rakha jabe nah. karon single data er khetre user er request e data deky
   });
   const blog = await res.json();
   console.log(blog);
