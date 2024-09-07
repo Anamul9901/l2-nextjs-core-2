df1980aa44ab76abd8976e70dd0fec7aea4c106f
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-07 13:07:02 +0600

    Home page data fetch

--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -1,9 +1,31 @@
-const LatestBlogs = () => {
+import { Blog } from "@/types";
+import LatestBlogCard from "../ui/LatestBlogCard";
+import BlogCard from "../ui/BlogCard";
+
+const LatestBlogs = ({ blogs }: { blogs: Blog[] }) => {
   return (
-    <div>
+    <div className="w-[90%] mx-auto">
       <h1 className="text-4xl text-center my-5">
         Latet Blogs From <span className="text-accent">Blogiz</span>
       </h1>
+      <p className="text-xl text-center text-gray-400 w-2/4 mx-auto">
+        Dive into the{" "}
+        <i>
+          fascinating world of quantum computing, where unlocking unprecedented
+          computational power.
+        </i>
+      </p>
+      <div className="grid grid-cols-2 gap-4 max-w-7xl mx-auto p-7 mt-5">
+        {blogs.slice(0, 2).map((blog) => (
+          <LatestBlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+
+      <div className="grid grid-cols-3 gap-4 max-w-7xl mx-auto p-7 mt-5">
+        {blogs.slice(2, 5).map((blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
     </div>
   );
 };
