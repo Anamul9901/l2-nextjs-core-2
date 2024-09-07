9310179f652e9d86f0da8be2a7131c9c5338cc9b
Anamul Haque <anamulhaque9901@gmail.com>
2024-09-07 12:52:41 +0600

    NestJs core 2

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
+
+const HomePage = async () => {
+  return (
+    <>
+      <LatestBlogs />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -0,0 +1,11 @@
+const LatestBlogs = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center my-5">
+        Latet Blogs From <span className="text-accent">Blogiz</span>
+      </h1>
+    </div>
+  );
+};
+
+export default LatestBlogs;
